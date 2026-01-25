0bee8ca2151fa71c69b64923d8b5ded0c26cca94
mi-araf <arafmushfiq@gmail.com>
2026-01-25 13:08:44 +0600

    pg 84 - ternary op done

---
+++ page 90/p4.js
@@ -0,0 +1,5 @@
+// তোর গাড়ির তেলের ট্যাঙ্ক যদি full হয়, তাহলে "Ready for a long drive" দেখাবি আর না হলে "Fill the tank" দেখাবি।
+
+const tankFull = true;
+
+tankFull === true ? console.log('ready for a long drive') : console.log('fill the tank');
---
+++ page 90/p7.js
@@ -0,0 +1,5 @@
+// যদি Expensive ভেরিয়েবলের মান false হয়, তাহলে কনসোল লগ করবি "I will buy this item."
+
+const Expensive = false;
+
+Expensive === true ? console.log('wont buy') : console.log('i will buy this item');

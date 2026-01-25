1e1b585699fbb474c844c90a905142610dd1a5ed
mi-araf <arafmushfiq@gmail.com>
2026-01-25 10:58:57 +0600

    page 84 if-else-if done

---
+++ page 84/t3.js
@@ -0,0 +1,5 @@
+var bankBalance = 900;
+
+if (bankBalance >= 5000) console.log('tui dhoni, amake biya kor');
+else if (bankBalance >= 1000 && bankBalance < 5000) console.log('bindash life enjoy kor');
+else console.log('deposit kor');
---
+++ page 84/t7.js
@@ -0,0 +1,5 @@
+const level = 50;
+
+if (level < 10) console.log('novice');
+else if (level >= 10 && level < 50) console.log('expert');
+else console.log('Pro Gamer');

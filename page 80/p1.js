ad3aa3dc34f0fb5ed10ef8bd0d935726135e3ac2
mi-araf <arafmushfiq@gmail.com>
2026-01-25 10:52:37 +0600

    page 80 if-else

---
+++ page 80/p1.js
@@ -0,0 +1,8 @@
+const age = 17;
+const height = 58;
+
+if (age >= 18 && height >= 60) {
+    console.log('ay gari thela de');
+} else {
+    console.log('boshe theko baba');
+}
---
+++ page 80/p3.js
@@ -0,0 +1,5 @@
+const gpa = 5;
+const monthlyIncome = 9000;
+
+if (gpa == 5 && monthlyIncome <= 10000) console.log('received scholarship!');
+else console.log('taka diye por');

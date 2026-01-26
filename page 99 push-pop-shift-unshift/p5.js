7e4080beafceea76353dbc0bccc1f10eb7994815
mi-araf <arafmushfiq@gmail.com>
2026-01-26 11:08:35 +0600

    pg 99 push-pop shift-unshift

---
+++ page 99 push-pop-shift-unshift/p5.js
@@ -0,0 +1,6 @@
+// তোর কাছে 5টা বইয়ের নাম দিয়ে একটা অ্যারে আছে। এখন তুই সবার প্রথম বইটা পড়ে শেষ করে ফেলছিস। তাই প্রথম বইয়ের নামটা অ্যারে থেকে রিমুভ করে ফেল। তারপর বাকি নামগুলো আউটপুট দেখ।
+
+const books = ['bigger nigger', 'small diller', 'marhaba js', 'tashpia bird', 'oliver brooze'];
+
+books.shift();
+console.log(books);
